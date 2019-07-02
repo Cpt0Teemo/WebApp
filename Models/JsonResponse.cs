4a0cf0de83baa3305dd1bde987d64381d022572b
Cpt0Teemo <ypoulmarck@gmail.com>
2019-07-01 22:12:37 -0400

    Create JsonResponse.cs

---
+++ Models/JsonResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class JsonResponse
+    {
+        public bool success { get; set; }
+
+        public Order order { get; set; }
+
+        public string message { get; set; }
+    }
+}
