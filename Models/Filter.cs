db9dcabe9d94328db7c93a79d5949e449413dfc5
Cpt0Teemo <ypoulmarck@gmail.com>
2019-07-10 22:09:21 -0400

    Added filters

---
+++ Models/Filter.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace WebApp.Models
+{
+    public class Filter
+    {
+        public enum FilterEntity
+        {
+            orderId,
+            name,
+            email,
+            createdOn,
+            excpectedDate,
+            comment,
+            done
+        }
+
+        public enum FilterType
+        {
+            exactString,
+            approximateString,
+            daterange,
+            exactDate
+        }
+
+        public string filterName { get; }
+
+        public FilterEntity filterEntity { get; }
+
+        public FilterType filterType { get; }
+        
+        public string value { get; set; }
+
+        public Filter(string filterName, FilterEntity filterEntity, FilterType filterType, string value = "")
+        {
+            this.filterName = filterName;
+            this.filterEntity = filterEntity;
+            this.filterType = filterType;
+            this.value = value;
+        }
+        
+    }
+
+    public class Filters
+    {
+        public List<Filter> filters = new List<Filter>();
+        
+        public Filters()
+        {
+            filters.Add(
+                new Filter(
+                    "OrderID (exact)",
+                    Filter.FilterEntity.orderId,
+                    Filter.FilterType.exactString));
+            
+            filters.Add(
+                new Filter(
+                    "Name (approx)",
+                    Filter.FilterEntity.name,
+                    Filter.FilterType.approximateString));
+            
+            filters.Add(
+                new Filter(
+                    "Email (approx)",
+                    Filter.FilterEntity.email,
+                    Filter.FilterType.approximateString));
+            
+            filters.Add(
+                new Filter(
+                    "CreatedOn (exact)",
+                    Filter.FilterEntity.createdOn,
+                    Filter.FilterType.exactDate));
+            
+            filters.Add(
+                new Filter(
+                    "ExpectedDate (exact)",
+                    Filter.FilterEntity.excpectedDate,
+                    Filter.FilterType.exactDate));
+            
+            filters.Add(
+                new Filter(
+                    "Date (exact)",
+                    Filter.FilterEntity.done,
+                    Filter.FilterType.exactDate));
+        }
+    }
+}
--- Repository.cs
+++ Repository.cs
@@ -9,6 +9,7 @@ namespace WebApp
     public class Repository
     {
         public OysterContext _context;
+        
         public Repository(OysterContext context)
         {
             _context = context;
@@ -18,6 +19,44 @@ namespace WebApp
         {
             _context.Add(order);
             _context.SaveChanges();
+        }
+
+        public List<Order> getOrders()
+        {
+            _context.Orders.Where()
+        }
+
+        public IQueryable<Order> addFiltersToQuery(IQueryable<Order> query, List<Filter> filters)
+        {
+            foreach (var filter in filters)
+            {
+                switch (filter.filterEntity)
+                {
+                    case Filter.FilterEntity.orderId:
+                        query.Where(x => x.orderId.ToString() == filter.value);
+                        break;
+                    case Filter.FilterEntity.email:
+                        query.Where(x => x.email == filter.value);
+                        break;
+                    case Filter.FilterEntity.name:
+                        query.Where(x => x.name == filter.value);
+                        break;
+                    case Filter.FilterEntity.createdOn:
+                        query.Where(x => x.createdOn == DateTime.Parse(filter.value));
+                        break;
+                    case Filter.FilterEntity.excpectedDate:
+                        query.Where(x => x.expectedDate == DateTime.Parse(filter.value));
+                        break;
+                    case Filter.FilterEntity.comment:
+                        query.Where(x => x.comment == filter.value);
+                        break;
+                    case Filter.FilterEntity.done:
+                        query.Where(x => x.done == DateTime.Parse(filter.value));
+                        break;
+                    
+                        
+                }
+            }
         }
     }
 }
